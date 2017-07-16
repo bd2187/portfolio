44651db5672d951feb13b203464bb2fcc17acb2a
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-07-16 12:47:11 -0700

    Add styling to NotFound component

--- app/components/NotFound.js
+++ app/components/NotFound.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="not-found content">
       <i className="fa fa-meh-o" aria-hidden="true"></i>
-      <h1>Sorry, page not found </h1>
+      <h1 className="not-found-title">Sorry, page not found </h1>
     </div>
   );
 }
