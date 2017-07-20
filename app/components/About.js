b50e4809df558794dc455095fe9ca11b83fc0d60
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-07-19 22:27:45 -0700

    Fix grid in about-me. Write 'about me' paragraphs

--- app/components/About.js
+++ app/components/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import profilePic from '../assets/sample-profile-pic.jpg';
 import profilePic from '../assets/brandon.jpg';
 
 function About() {
@@ -7,21 +6,27 @@ function About() {
     <div className="about content">
       <h1 className="category-title">About Me</h1>
       <div className="about-box clearfix">
-        <img className='profile-photo' src={profilePic} alt="Profile Photo"/>
-        <p className="about-me">
-          {`Hi! I'm Brandon, a self-taught Front End Developer based in Los Angeles, CA.
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. You can contact me at at `}
-          <a href="mailto:brandon.dionisio@yahoo.com">
-            {"brandon.dionisio.26@gmail.com"}
-          </a>{"."}
-        </p>
+        <div className="about-me-box">
+          <img className='profile-photo' src={profilePic} alt="Profile Photo"/>
+          <div className="about-me">
+            <p>
+              {"Hi! I'm Brandon, a self-taught Front End Developer based in Los Angeles, CA. I've learned how to program through various resources, most notably "}<a href="https://www.freecodecamp.com" target="_blank">Freecodecamp</a>{", "}<a href="https://teamtreehouse.com/" target="_blank">TeamTreehouse</a>{", and "}<a href="https://www.udemy.com/" target="_blank">Udemy</a>{". The opportunity to practice and develop my problem-solving and creativity skills brought me to the tech industry. My enthusiasm and desire to learn fuels my passion to build and maintain applications."}
+            </p>
+            <p>
+              {"I'm currently seeking a Junior Developer position to practice my skills and grow as a developer. You can contact me at "}
+              <a href="mailto:brandon.dionisio@yahoo.com">
+                {"brandon.dionisio.26@gmail.com"}
+              </a>{"."}
+            </p>
+          </div>
+        </div>
         <div className="skills-box">
           <h3 className="skillsHeader">Skills:</h3>
           <ul className="skillsList">
             <li>HTML5</li>
             <li>CSS3</li>
             <li>Sass</li>
-            <li>Responsive Layouts</li>
+            <li>Responsive Design</li>
             <li>JavaScript</li>
             <li>AJAX</li>
             <li>React JS</li>
